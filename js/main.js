37974c6ee1b4ad960a23ad090568a59bfc9103ad
Noah <noahsemus@gmail.com>
2019-04-17 17:04:15 -0400

    pls

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 var transitionAnimation = Barba.BaseTransition.extend({
     
   start: function() {
-    /**
-     * This function is automatically called as soon the Transition starts
-     * this.newContainerLoading is a Promise for the loading of the new container
-     * (Barba.js also comes with an handy Promise polyfill!)
-     */
-
-    // As soon the loading is finished and the old page is faded out, let's fade the new page
     Promise
       .all([this.newContainerLoading, this.startTransition()])
       .then(this.fadeIn.bind(this));
@@ -16,9 +9,7 @@ var transitionAnimation = Barba.BaseTransition.extend({
   startTransition: function() {
       
       var transitionPromise = new Promise(function(resolve) {
-    /**
-     * this.oldContainer is the HTMLElement of the old Container
-     */
+
   var outTransition = new TimelineMax();
       
       outTransition
@@ -33,11 +24,7 @@ var transitionAnimation = Barba.BaseTransition.extend({
   },
 
   fadeIn: function() {
-    /**
-     * this.newContainer is the HTMLElement of the new Container
-     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-     * Please note, newContainer is available just after newContainerLoading is resolved!
-     */
+
 
     var _this = this;
     var $el = $(this.newContainer);
@@ -53,18 +40,8 @@ var transitionAnimation = Barba.BaseTransition.extend({
     
 });
 
-/**
- * Next step, you have to tell Barba to use the new Transition
- */
 
 Barba.Pjax.getTransition = function() {
-    
-  /**
-   * Here you can use your own logic!
-   * For example you can use different Transition based on the current page or link...
-   */
-    
-    
 
   return transitionAnimation;
 };
@@ -75,7 +52,7 @@ function vendorStart() {
     
     new TypeIt('.deepLink', {
         speed: 50,
-        waitUntilVisible: true,
+        waitUntilVisible: false,
         cursor: false,
         loop: true,
         loopDelay: 5000,
@@ -93,6 +70,9 @@ function vendorStart() {
 vendorStart();
 
 
-Barba.Dispatcher.on('transitionCompleted', function(currentStatus, oldStatus, container) {
+Barba.Dispatcher.on('transitionCompleted', function(currentStatus, oldStatus, container, rawHTML) {
+    
+    $(document).foundation();
+    
     vendorStart();
 });
